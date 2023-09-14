9b317c735cf514d0dc1c834dd61e5825ac4e4678
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-14 14:12:13 +0800

    Initial Commit

---
+++ hotchocolate/hotchocolateApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotchocolateApp.swift
+//  hotchocolate
+//
+//  Created by Jia Chen Yee on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct hotchocolateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
